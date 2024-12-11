0b6a3c511b682c1d078fbdb61cf3aba521503871
harontaiko <harontaiko@gmail.com>
2024-12-11 16:57:06 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Aspose.Email;
+using Aspose.Email.Mapi;
+using Aspose.Email.Storage.Pst;
+
+/**change to desired directories for input/output**/
+/**May create an executable for Windows,Mac and Linux**/
+var dirPath = @"C:\Users\user\Desktop\Sent";
+var outFileName = @"C:\Users\user\Desktop\outputFile.pst";
+
+if (File.Exists(outFileName))
+{
+    File.Delete(outFileName);
+}
+
+using (var personalStorage = PersonalStorage.Create(outFileName, FileFormatVersion.Unicode))
+{
+    var inboxFolder = personalStorage.RootFolder.AddSubFolder("Inbox");
+
+    foreach (var f in Directory.GetFiles(dirPath, "*.eml"))
+    {
+        using (var message = MailMessage.Load(f))
+        {
+            inboxFolder.AddMessage(MapiMessage.FromMailMessage(message, MapiConversionOptions.UnicodeFormat));
+            Console.WriteLine($"Added File: {Path.GetFileName(f)}");
+        }
+    }
+}
+
+Console.WriteLine("Conversion Done!");
